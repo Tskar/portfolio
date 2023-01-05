d6041b6b08e4d103dc8745ae0fd47d00744f4b87
Tskar <sanskarthapaofficial@gmail.com>
2023-01-04 19:54:06 -0500

    scroll animation.

--- pages/Intro.js
+++ pages/Intro.js
@@ -1,5 +1,7 @@
-import {AiFillGithub, AiFillInstagram, AiOutlineMail} from 'react-icons/ai';
 import Link from 'next/link';
+
+import {AiFillGithub, AiFillInstagram, AiOutlineMail} from 'react-icons/ai';
+import {FaAngleDown} from 'react-icons/fa';
 
 import { motion } from "framer-motion";
 
@@ -7,21 +9,20 @@ import { motion } from "framer-motion";
 export default function Intro() {
 
     return (
-        <div className="min-h-screen flex flex-col text-center justify-center gap-5">
-
+        <div className="relative min-h-screen flex flex-col text-center justify-center gap-4">
             <div className='max-w-8xl mx-auto'>
                 <h1 className="text-3xl drop-shadow-md tracking-[10px] text-white 
                     md:drop-shadow-xl md:text-7xl lg:text-9xl lg:drop-shadow-lg dark:text-gray-800 ">
                     programmer
                 </h1>
             </div>
-            <div>
+            <div className=''>
                 <h2 className="uppercase tracking-[20px] text-gray-500 text-lg mt-2 
                     md:py-4 lg:py-4 md:text-xl lg:text-2xl  dark:text-gray-300">
                     Sanskar Thapa
                 </h2>
             </div>
-            <div>
+            <div className=''>
                 <h2 className="text-xl py-2 text-gray-800 font-medium 
                     md:text-3xl lg:text-4xl dark:text-gray-400">
                     Software Developer.
@@ -42,11 +43,10 @@ export default function Intro() {
             </div>
 
             <motion.div 
-              initial={{y:-20, opacity: 0, scale: 0.5}}
-              animate={{}}
+              initial={{y:-10, opacity: 0, scale: 0.8}}
               transition={{duration:.5}}
               whileInView={{opacity: 1, y:0, scale:1}}
-              className="text-3xl flex justify-center gap-16 py-3 text-gray-600 md:text-4xl lg:text-6xl dark:text-gray-300">
+              className="text-3xl flex justify-center gap-16 py-3 mb-4 text-gray-600 md:text-4xl lg:text-6xl dark:text-gray-300">
               <motion.div whileHover={{scale:1.2}}>
                 <a href="https://github.com/Tskar">
                     <AiFillGithub />
@@ -63,6 +63,13 @@ export default function Intro() {
                 </Link>
               </motion.div>
             </motion.div>
+
+            <div className='down-anim s-down-anim dark:text-white'>
+                <FaAngleDown className='dwnicn' />
+                <FaAngleDown className='dwnicn' />
+                <FaAngleDown className='dwnicn' />
+            </div>
+            
         </div>
     );
 }
